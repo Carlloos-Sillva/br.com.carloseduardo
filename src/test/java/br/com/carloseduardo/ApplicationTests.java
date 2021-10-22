af677df2693ff08b8074c95a7e945ed0599dc7ba
Carlos Eduardo da Silva <80638432+Carlloos-Sillva@users.noreply.github.com>
2021-10-22 12:22:20 -0300

    Versao 01.00
    Projeto web

---
+++ src/test/java/br/com/carloseduardo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carloseduardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
