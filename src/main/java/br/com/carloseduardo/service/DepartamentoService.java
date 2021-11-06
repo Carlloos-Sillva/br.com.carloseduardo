eecfd29f3a7c86d681f5b8e83e0a78f9ee07cf40
Carlos Eduardo da Silva <80638432+Carlloos-Sillva@users.noreply.github.com>
2021-11-06 09:17:38 -0300

    Versão 01.05
    Update com recursos de fragments e layout.

---
+++ src/main/java/br/com/carloseduardo/service/DepartamentoService.java
@@ -0,0 +1,18 @@
+package br.com.carloseduardo.service;
+
+import java.util.List;
+
+import br.com.carloseduardo.domain.Departamento;
+
+public interface DepartamentoService {
+
+    void salvar(Departamento departamento);
+
+    void editar(Departamento departamento);
+
+    void excluir(Long id);
+
+    Departamento buscarPorId(Long id);
+    
+    List<Departamento> buscarTodos();
+}
---
+++ src/main/java/br/com/carloseduardo/service/FuncionarioService.java
@@ -0,0 +1,18 @@
+package br.com.carloseduardo.service;
+
+import java.util.List;
+
+import br.com.carloseduardo.domain.Funcionario;
+
+public interface FuncionarioService {
+
+    void salvar(Funcionario funcionario);
+
+    void editar(Funcionario funcionario);
+
+    void excluir(Long id);
+
+    Funcionario buscarPorId(Long id);
+
+    List<Funcionario> buscarTodos();
+}
